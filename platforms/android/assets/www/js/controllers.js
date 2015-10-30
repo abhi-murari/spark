f6fce98f77fd86c996d7c3e663484fb02189b16a
abhi-murari <abilashm3456@gmail.com>
2015-10-30 05:07:44 -0700

    added maps

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('ordersAndRequestsCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+      
+.controller('orderDetailsCtrl', function($scope) {
+
+})
+   
+.controller('trackTechnicianCtrl', function($scope) {
+
+})
+   
+.controller('serviceRequest#78721737Ctrl', function($scope) {
+
+})
+ 
